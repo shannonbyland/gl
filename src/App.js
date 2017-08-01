86b4ec2848d90c061ce1d129548d106c94e7bae3
Shannon Byland <shannonbyland@gmail.com>
2017-07-31 21:25:51 -0600

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Route exact path="/" component={Home} />
+)
 
 export default App;
---
+++ src/components/Gallery.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Gallery = () => (
+
+)
+
+
+export default Gallery;

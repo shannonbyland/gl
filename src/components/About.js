fb41a80efb0deae882f741ad44e59c8c30dabcee
Shannon Byland <shannonbyland@gmail.com>
2017-08-08 21:20:54 -0600

    working on about

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 const About = () => (
   <h1>About</h1>
+  <p>
+    Gravity Logic has been founded by the team behind the Whistler Mountain Bike Park’s success. 
+    In fact, each Gravity Logician is still actively involved in the continuing development
+    of Whistler’s Park. Clearly not a bunch of out of touch consultants.
+  </p>
 )
 
 
